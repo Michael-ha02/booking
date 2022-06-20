ca54915b3875152c482ea5b4457508f2fbe0f2d8
Michael Ha <michaelhabao@gmail.com>
2022-06-20 14:06:51 +0700

    second commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../header/Header";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
